19e571d9789b9810081f490de51befd4558704a1
JoleenTsai <joleenjtsai@gmail.com>
2018-08-15 07:34:49 -0700

    removed extra index

---
+++ index.js
@@ -0,0 +1,66 @@
+let score = 0
+let currentWord = ""
+let inGuesses = 0
+let guessLeft = 10
+let currentGuesses = []
+let wrongLetters = []
+
+function hiddenWord() {
+    let tmpWord = []
+    for(let i = 0; i < currentWord.length; i++)
+        if(currentGuesses.indexOf(currentWord[i]) !== -1) {
+            tmpWord.push(currentWord[i])
+        } else {
+            tmpWord.push("*")
+        }
+}
+
+const allWords = []
+
+function ranWord(arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
+}
+
+function restartRound() {
+    currentWord = ranWord(allWords)
+    guessLeft = 10
+    wrongLetters = []
+}
+
+function displayValues() {
+    let scoreT = document.querySelector("#score")
+    let hword = document.querySelector("#hiddenWord")
+    let gleft = document.querySelector("#guessLeft")
+    let wrLet = document.querySelector("#wrongLetters")
+
+    scoreT.innerHTML = score
+    hword.innerHTML = hiddenWord()
+    gleft.innerHTML = guessLeft
+    wrLet.innerHTML = wrongLetters.join()
+
+}
+
+document.addEventListener("keypress", function(event) {
+    const letter = event.key.toLowerCase()
+    currentGuesses.push(letter)
+
+    if(guessLeft <= 0) {
+        restartRound()
+    }
+
+    if(hiddenWord() === currentWord) {
+        score++
+        restartRound()
+        return
+    }
+
+    if(currentWord.indexOf(letter) !== -1) {
+        
+    } else {
+        guessLeft-- 
+        wrongLetters.push(letter)
+    }
+    displayValues()
+})
+
+restartRound()
